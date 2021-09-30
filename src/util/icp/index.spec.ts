878f4e847157625e56fb604356517e19ca5b8d8b
earthgohan <85003405+earthgohan@users.noreply.github.com>
2021-09-30 19:53:44 +0530

    Add EXT NFTokens get balance,list and transfer with tests

---
+++ src/util/icp/index.spec.ts
@@ -0,0 +1,71 @@
+import test from 'ava';
+
+import { createWallet } from '@earthwallet/keyring';
+import { getNFTsFromCanisterExt, transferNFTsExt, listNFTsExt } from '.';
+
+test('get tokens for a EXT type canister for a user', async (t) => {
+  try {
+    const tokens = await getNFTsFromCanisterExt(
+      'owuqd-dyaaa-aaaah-qapxq-cai',
+      '0ba1b7b1643929210dc41a8afbe031bd1b5e81dbc8e3b3b64978f5f743f058c3'
+    );
+
+    t.like(tokens[0], {
+      metadata: [],
+      info: {
+        seller:
+          'o7nwu-n6kuf-4afzp-ybcuf-346pr-odd54-damf5-v4pvc-4sexh-cabph-7qe',
+        price: BigInt(18000000000),
+        locked: [],
+      },
+      tokenIndex: 2112,
+      tokenIdentifier: '5pzgh-likor-uwiaa-aaaaa-b4ad5-4aqca-aabba-a',
+      forSale: true,
+    });
+  } catch (error) {
+    console.log(error);
+    t.truthy(false);
+  }
+});
+
+test('transfer saleable NFT of a EXT canister should give TOKEN_LISTED_FOR_SALE status', async (t) => {
+  try {
+    const seedPhrase =
+      'open jelly jeans corn ketchup supreme brief element armed lens vault weather original scissors rug priority vicious lesson raven spot gossip powder person volcano';
+
+    const walletObj = await createWallet(seedPhrase, 'ICP');
+
+    const status = await transferNFTsExt(
+      'owuqd-dyaaa-aaaah-qapxq-cai',
+      walletObj.identity,
+      '0ba1b7b1643929210dc41a8afbe031bd1b5e81dbc8e3b3b64978f5f743f058c3',
+      '2112'
+    );
+
+    t.is(status, 'TOKEN_LISTED_FOR_SALE');
+  } catch (error) {
+    console.log(error);
+    t.truthy(false);
+  }
+});
+
+test('list not owned NFT of a canister should give UNAUTHORISED status', async (t) => {
+  try {
+    const seedPhrase =
+      'open jelly jeans corn ketchup supreme brief element armed lens vault weather original scissors rug priority vicious lesson raven spot gossip powder person volcano';
+
+    const walletObj = await createWallet(seedPhrase, 'ICP');
+
+    const status = await listNFTsExt(
+      'owuqd-dyaaa-aaaah-qapxq-cai',
+      walletObj.identity,
+      '2112',
+      4
+    );
+
+    t.is(status, 'UNAUTHORISED');
+  } catch (error) {
+    console.log(error);
+    t.truthy(false);
+  }
+});
